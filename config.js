cd361f34ec13178c78c41b92688c2e20189ae344
Simon Cheng <simoncheng559@gmail.com>
2017-12-24 22:52:31 +0000

    Updated Readme

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var config = {
     // Sets the width and height of the browser
     // Be sure not to include "" else javascript will think this is a string
-    "width": 1024,
-    "height": 768,
+    "width": 1,
+    "height": 1,
     
     // Default height width //true or false
-    "defaultSize": true,
+    "defaultSize": false,
     
     // This enters the username/email and password for c9.io
-    "username": "",
-    "password": "",
+    "username": "simoncheng",
+    "password": "violinviolapiano",
     
     // Location of the ide to "keep alive", usually when you go to the ide, the url is it
-    "ideLocation": "", //Example: https://ide.c9.io/USERNAME/PROJECT
+    "ideLocation": "https://ide.c9.io/simoncheng/self-running", //Example: https://ide.c9.io/USERNAME/PROJECT
     // Take a screenshot to see its working true = take screenshot, false = don't take screenshot
     // it has to be either true or false
     "screenshot": false
