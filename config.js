81b80890ed318496cb8dd5d715dcb44b345789c0
Simon Cheng <simoncheng559@gmail.com>
2017-12-24 23:11:10 +0000

    Updated Readme

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ var config = {
     "defaultSize": false,
     
     // This enters the username/email and password for c9.io
-    "username": "simoncheng",
-    "password": "violinviolapiano",
+    "username": "",
+    "password": "",
     
     // Location of the ide to "keep alive", usually when you go to the ide, the url is it
-    "ideLocation": "https://ide.c9.io/simoncheng/self-running", //Example: https://ide.c9.io/USERNAME/PROJECT
+    "ideLocation": "", //Example: https://ide.c9.io/USERNAME/PROJECT
     // Take a screenshot to see its working true = take screenshot, false = don't take screenshot
     // it has to be either true or false
     "screenshot": false
